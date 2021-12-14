d62824f82874b7de39c5c354614abf0910a25d1f
Ronnie Dutta <61982285+MetRonnie@users.noreply.github.com>
2021-12-14 15:19:37 +0000

    fixup! Create custom 404 page with redirect ability

--- etc/404.js
+++ etc/404.js
@@ -1,90 +1,52 @@
-const REDIRECT_DELAY = 1000;
-
-const REVERSE_MAPPING = {
-    'introduction/index.html': ['introduction.html'],
-
-    'glossary.html': ['terminology.html'],
-
-    'tutorial/index.html': ['tutorial.html'],
-
-    'user-guide/writing-workflows/index.html': [
-        'user-guide/writing-suites.html',
-        'suite-config.html',
-    ],
-    'user-guide/task-implementation/index.html': [
-        'task-implementation.html',
-    ],
-    'user-guide/task-implementation/job-submission.html': [
-        'task-job-submission.html',
-    ],
-    'user-guide/task-implementation/ssh-job-management.html': [
-        'appendices/remote-job-management.html'
-    ],
-    'user-guide/writing-workflows/external-triggers.html': [
-        'external-triggers.html'
-    ],
-    'user-guide/running-workflows.html': [
-        'user-guide/running-suites.html',
-        'running-suites.html',
-    ],
-
-    'reference/index.html': [
-        'appendices/appendices-master.html',
-    ],
-    'reference/config/workflow.html': [
-        'reference/config/suite.html',
-        'appendices/suiterc-config-ref.html',
-    ],
-    'reference/config/global.html': [
-        'appendices/site-user-config-ref.html',
-    ],
-
-    'workflow-design-guide/index.html': [
-        'suite-design-guide/index.html',
-        'suite-design-guide/suite-design-guide-master.html',
-    ],
-    'workflow-design-guide/style-guide.html': [
-        'suite-design-guide/style-guide.html'
-    ],
-    'workflow-design-guide/general-principles.html': [
-        'suite-design-guide/general-principles.html'
-    ],
-    'workflow-design-guide/efficiency.html': [
-        'suite-design-guide/efficiency.html'
-    ],
-    'workflow-design-guide/portable-workflows.html': [
-        'suite-design-guide/portable-suites.html'
-    ]
-};
-
-function performDelayedRedirect(dest) {
-    const redirectLink = document.getElementById('redirect-link');
-    redirectLink.setAttribute('href', dest);
-    window.setTimeout(() => {
-        window.location.replace(window.location.origin + dest);
-    }, REDIRECT_DELAY);
+/**
+ * Returns the version string from the URL pathname, if applicable.
+ * @param {string} base - The base of the pathname, usually '/cylc-doc/'
+ * @param {string} path - The pathname from the URL
+ * @returns {string|undefined} version
+ */
+ function getVersion(base, path) {
+    if (!path.startsWith(base)) {
+        return;
+    }
+    path = path.replace(base, '');
+    let pathArr = path.split('/');
+    if (pathArr.length < 2 || pathArr[1] !== 'html') {
+        return;
+    }
+    return pathArr[0];
 }
 
+
+/**
+ * Update the HTML with the Cylc version garnered from the URL.
+ * @param {string} version
+ */
+function setVersionInHTML(version) {
+    document.getElementById('home-button')
+        .setAttribute('href', `${version}/html/index.html`);
+    document.querySelectorAll('.version-num').forEach((e) => {
+        e.innerHTML = version;
+    });
+}
+
+
 window.onload = () => {
-    const body = document.getElementsByTagName('body')[0];
+    const body = document.querySelector('body');
     body.classList.remove('no-js');
 
-    // Redirect if appropriate
+    // Try and get version from URL
+    const base = document.querySelector('base').getAttribute('href');
     let path = window.location.pathname;
-    for (const dest in REVERSE_MAPPING) {
-        for (const tail of REVERSE_MAPPING[dest]) {
-            if (path.endsWith(tail)) {
-                body.classList.add('redirecting');
-                path = path.replace(tail, dest);
-                if (window.location.search) {
-                    path += window.location.search;
-                }
-                if (window.location.hash) {
-                    path += window.location.hash;
-                }
-                performDelayedRedirect(path);
-                return;
+
+    const version = getVersion(base, path);
+    if (!version) return;
+
+    // Check against existing versions
+    fetch(`${base}versions.json`, {method: 'GET'})
+        .then((response) => response.json())
+        .then((versions) => {
+            if (version in versions) {
+                setVersionInHTML(version);
             }
-        }
-    }
+        });
 };
